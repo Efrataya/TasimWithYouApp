f07697cbc5df4376f91823c5583d6012b47b9984
Efrataya <efrataya@gmail.com>
2023-02-22 21:49:08 +0200

    Connected the pages we have already

--- app/src/main/java/com/example/tasimwithyouapp/FragmentRegister.java
+++ app/src/main/java/com/example/tasimwithyouapp/FragmentRegister.java
@@ -3,10 +3,13 @@ package com.example.tasimwithyouapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +62,17 @@ public class FragmentRegister extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View view= inflater.inflate(R.layout.fragment_register, container, false);
+        Button button=view.findViewById(R.id.registerButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_fragmentRegister_to_fragmentmenu);
+            }
+        });
+
+
+        return view;
     }
+
 }
